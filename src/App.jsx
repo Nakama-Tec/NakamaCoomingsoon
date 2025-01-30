6ae90ed6f95b8edd3adbdb6fcbf8a5748269faad
IsmaDevs <chavezpedrito04@gmail.com>
2025-01-30 02:18:32 -0300

    uploaded files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import Contador from "./components/Contador";
-import { Link } from "react-router-dom";
 import { FaInstagram } from "react-icons/fa";
 import { FaThreads } from "react-icons/fa6";
 import { FaXTwitter } from "react-icons/fa6";
@@ -8,30 +7,33 @@ function App() {
   return (
     <div className="flex flex-col items-center justify-center h-screen text-white text-center p-4">
       <div className="absolute left-4 top-1/2 transform -translate-y-1/2 flex flex-col space-y-4">
-        <Link
-          to="https://www.instagram.com/nakama.tec/"
+        <div>
+        </div>
+        <a
+          href="https://www.instagram.com/nakama.tec/"
           target="_blank"
-          href="#"
+          rel="noopener noreferrer"
           className="text-white text-xl border border-white p-2 transition hover:bg-gradient-to-r hover:from-yellow-400 hover:via-pink-500 hover:to-purple-600 hover:opacity-80"
         >
           <FaInstagram />
-        </Link>
-        <Link
-          to="https://www.threads.net/@nakama.tec"
+        </a>
+        <a
+          href="https://www.threads.net/@nakama.tec"
           target="_blank"
+          rel="noopener noreferrer"
           className="text-white text-xl border border-white p-2 hover:bg-gray-900 transition"
         >
           <FaThreads />
-        </Link>
+        </a>
         <a
-          href="#"
+          to="/"
           className="text-white text-xl border border-white p-2 hover:bg-black transition"
         >
           <FaXTwitter />
         </a>
       </div>
       <div className="text-center">
-        <p className="text-4xl md:text-5xl font-light tracking-wide uppercase relative top-4 font-semibold">
+        <p className="text-4xl md:text-5xl tracking-wide uppercase relative top-4 font-semibold">
           MUY
         </p>
         <p className="text-5xl md:text-7xl font-bold uppercase mt-2 relative top-5">
